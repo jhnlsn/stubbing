7ec6ac517d56ba50c6c9f3aa318e13b64305324b
jhnlsn <jnelson11@gmail.com>
2017-04-24 15:30:53 -0400

    added some assertions

--- test/Zoo.js
+++ test/Zoo.js
@@ -2,20 +2,24 @@ let Zoo = require("../lib/Zoo");
 let sinon = require("sinon");
 let mysql = require("promise-mysql");
 let Promise = require('bluebird');
+const assert = require('assert');
 
 describe('Zoo', function() {
-  it('should stubb', function(done) {
-
-
+  it('should stubb 1 animal', function(done) {
     let bronx = new Zoo();
     
     let stub = sinon.stub(bronx.conn, 'query').callsFake(function(){
+      // Could load a fixture and return it instead of my half ass attempt
       return Promise.resolve([{
         name: 'Zebra'
       }]);
     });
 
-    bronx.animals();
-    done();
+    let all = bronx.animals();
+
+    all.then(animals => {
+      assert.equal(animals.length, 1, 'Animals function should return one result');
+      assert.equal(animals[0].name, 'Zebra');
+    }).then(done);
   });
 })
